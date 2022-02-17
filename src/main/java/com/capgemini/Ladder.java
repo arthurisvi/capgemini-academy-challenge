4111e683893d5f8c0d96f144b1768765e2b6955e
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-02-17 00:04:07 -0300

    small visual changes

--- src/main/java/com/capgemini/Ladder.java
+++ src/main/java/com/capgemini/Ladder.java
@@ -3,9 +3,6 @@ package com.capgemini;
 public class Ladder {
     static void printLadder(int n){
         int i = 0;
- 
-        System.out.println("Questão 1: ");
-        System.out.println("------------------------------");
         while(i <= n){
              for(int j = 1;j <= n-i;j++){
                     System.out.print("  ");
@@ -16,6 +13,5 @@ public class Ladder {
              i++;
             System.out.println();
       }
-         System.out.println("------------------------------");
      }
 }
