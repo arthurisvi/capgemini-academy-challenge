8abb42fd3487936be6e654a14768b58078d4c130
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-02-18 13:20:39 -0300

    adding new test cases

--- src/test/java/com/capgemini/AppTest.java
+++ src/test/java/com/capgemini/AppTest.java
@@ -15,6 +15,22 @@ public class AppTest
         assertEquals(0, testResult);
     }
 
+    //Caso 1 de teste no contador de anagrama
+    @Test
+    public void countAnagramsOneCase(){
+        int testResult = Anagram.getAnagramsQuantity("ovo");
+
+        assertEquals(2, testResult);
+    }
+
+    //Caso 2 de teste no contador de anagrama
+    @Test
+    public void countAnagramsTwoCase(){
+          int testResult = Anagram.getAnagramsQuantity("ifailuhkqq");
+  
+          assertEquals(3, testResult);
+    }
+
     //A soma dos caracteres necessários para criar a senha forte + o tamanho da senha deve ser maior ou igual a 6
     @Test
     public void requiredCharactersMinSix(){
@@ -24,7 +40,16 @@ public class AppTest
         assertTrue(countCharacters + password.length() >= 6);
     }
 
-    //A quantidade de vezes que o loop roda deve ser igual ao tamanho (n) + 1
+      //Caso 1 de teste no contador de caracteres para senha forte
+      @Test
+      public void requiredCharactersCase(){
+          String password = "Ya3";
+          int countCharacters = Password.validatePassword(password);
+  
+          assertEquals(3, countCharacters);
+      }
+
+    //A quantidade de vezes que o loop roda deve ser igual ao tamanho (n) + 1, para representar a altura do triangulo
     @Test 
     public void countLoop(){
         int n = 6;
