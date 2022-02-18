4a3ba9bac646cba58a4535a3e1cb74984ecc245d
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-02-17 22:13:11 -0300

    implementing data insertion via user input

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -1,4 +1,5 @@
 package com.capgemini;
+import java.util.Scanner;
 /**
 * Classe principal da aplicação
 * @author Arthur Isvi
@@ -8,13 +9,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Questão 1: ");
-        System.out.println("------------------------------");
-        Ladder.printLadder(6);
-        System.out.println("------------------------------");
-        System.out.println("Questão 2: " + Password.validatePassword("1eeeeeDee"));
-        System.out.println("------------------------------");
-        System.out.println("Questão 3: " + Anagram.getAnagramsQuantity("ovo"));
-        System.out.println("------------------------------");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Questão 1 - Digite o n (tamanho da base e altura) da escada: ");
+        int n = input.nextInt();
+        System.out.println("Saída:");
+        Ladder.printLadder(n);
+        System.out.print("Questão 2 - Digite a senha: ");
+        String password = input.next();
+        System.out.println("Saída: " + Password.validatePassword(password));
+        System.out.print("Questão 3 - Digite a palavra desejada: ");
+        String word = input.next();
+        System.out.println("Saída: " + Anagram.getAnagramsQuantity(word));
+
+        input.close();
     }
 }
